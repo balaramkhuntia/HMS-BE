6e5374f6fd6db31f2f3019a2e84f3bb350ae8045
sanju <s4njua@gmail.com>
2024-02-25 07:48:27 +0530

    Security Issue Fixed

--- src/main/java/com/hms/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/hms/security/JwtAuthenticationEntryPoint.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
@@ -19,21 +19,25 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-	  @Override
-	  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-	      throws IOException, ServletException {
-	    log.error("Unauthorized error: {}", authException.getMessage());
 
-	    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-	    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-
-	    final Map<String, Object> body = new HashMap<>();
-	    body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
-	    body.put("error", "Unauthorized");
-	    body.put("message", authException.getMessage());
-	    body.put("path", request.getServletPath());
-
-	    final ObjectMapper mapper = new ObjectMapper();
-	    mapper.writeValue(response.getOutputStream(), body);
-	  }
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException)
+			throws IOException, ServletException {
+		log.error("Unauthorized error: {}", authException.getMessage());
+		
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		
+		final Map<String, Object> body = new HashMap<>();
+		body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+		body.put("error", "Unauthorized");
+		body.put("message", authException.getMessage());
+		body.put("path", request.getServletPath());
+		
+		final ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(response.getOutputStream(), body);
+		// TODO Auto-generated method stub
+		
+	}
 }
